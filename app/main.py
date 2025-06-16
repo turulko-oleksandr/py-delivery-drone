99087be922dcab8aefe793efff16a4131981f2c4
turulko-oleksandr <SashaTurulko@i.ua>
2025-06-16 12:52:08 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Optional
 
 
 class Cargo:
@@ -9,7 +9,7 @@ class Cargo:
 class BaseRobot:
     def __init__(self, name: str,
                  weight: float,
-                 coords: List[int] = None) -> None:
+                 coords: Optional[list] = None) -> None:
         self.weight = weight
         self.name = name
 
@@ -37,7 +37,7 @@ class BaseRobot:
 class FlyingRobot(BaseRobot):
     def __init__(self, name: str,
                  weight: float,
-                 coords: List[int] = None) -> None:
+                 coords: Optional[list] = None) -> None:
         if coords is None:
             coords = [0, 0, 0]
         super().__init__(name, weight, coords)
@@ -52,8 +52,8 @@ class FlyingRobot(BaseRobot):
 class DeliveryDrone(FlyingRobot):
     def __init__(self, name: str, weight: float,
                  max_load_weight: int,
-                 current_load: Cargo,
-                 coords: List[int] = None) -> None:
+                 current_load: Cargo = None,
+                 coords: Optional[list] = None) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
         self.current_load = current_load
